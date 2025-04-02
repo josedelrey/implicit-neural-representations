0a109b6fe805159cc64ff6ec462941add4505cba
josedelrey <josedelreyortega@gmail.com>
2025-04-02 13:30:23 +0200

    Refactor encoding functions

--- modules/wire.py
+++ modules/wire.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 from collections import OrderedDict
 
-from encoding import positional_encoding
+from modules.encoding import positional_encoding
 
 
 class RealGaborLayer(nn.Module):
