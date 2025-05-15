7cf0dfb29a6be232ffed1dfbdbc86c025a7a1f2e
josedelrey <josedelreyortega@gmail.com>
2025-05-15 21:55:50 +0200

    implement omega0 estimation script

--- video.py
+++ video.py
@@ -29,7 +29,7 @@ def main():
     log_interval = 10
     batch_size = 32768
     chunk_size = 1024
-    model_type = 'experiment'
+    model_type = 'waveletnetnormalized' # 28.5 PSNR for gabornet
 
     # Data
     dataset = VideoDataset(sidelength, path=video_path, channels=channels)
